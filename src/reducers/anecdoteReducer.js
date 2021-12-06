5f6e34a1e7ada568fc1aff5273da1134a6deff77
David Puerta Martos <dpuerta@usal.es>
2021-12-06 15:26:32 +0100

    6.14

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -20,12 +20,14 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
     switch(action.type){
       case 'NEW_ANECDOTE':
         return [...state, action.data]
+      case 'INIT_ANECDOTES':
+        return action.data  
       case 'VOTE': {
         const id = action.data.id
         const anecdoteToVote = state.find(anecdote => anecdote.id === id)
@@ -43,14 +45,17 @@ export const addVoteOf = (id) => {
   }
 }
 
-export const createAnecdote = (content1) => {
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes,
+  }
+}
+
+export const createAnecdote = (data) => {
   return {
       type: 'NEW_ANECDOTE',
-      data: {
-          content: content1,
-          id: getId(),
-          votes: 0
-      }
+      data,
   }
 }
 

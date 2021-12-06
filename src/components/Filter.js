9b21968ae7c3b447ece6316c1defa7db968b2184
David Puerta Martos <dpuerta@usal.es>
2021-12-06 20:36:52 +0100

    6.20.1

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { filterChange } from '../reducers/filterReducer'
 
-const Filter = () => {
-  const dispatch = useDispatch()
+const Filter = (props) => {
   const handleChange = (event) => {
-    dispatch(filterChange(event.target.value))
+    props.filterChange(event.target.value)
     // input-field value is in variable event.target.value
   }
   const style = {
@@ -19,4 +18,16 @@ const Filter = () => {
   )
 }
 
-export default Filter
+const mapDispatchToProps = {
+  filterChange,
+}
+
+const mapStateToProps = (state) => {
+  return {
+    filter: state.filter,
+  }
+}
+
+const ConnectedFilter = connect(mapStateToProps, mapDispatchToProps)(Filter)
+
+export default ConnectedFilter
